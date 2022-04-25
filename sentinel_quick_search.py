d45f6cda6b165c777d7ebc8d38f4e79d1e43fe88
Roger Almengor Gonzalez <roger.almengor@gaf.de>
2022-04-25 17:11:02 +0200

    2do:managing default text, center left elems

--- sentinel_quick_search.py
+++ sentinel_quick_search.py
@@ -34,28 +34,27 @@ class App(ctk.CTk):
 
     def __init__(self, *args, **kwargs):
 
-        #self.master = master
         super().__init__(*args, **kwargs)
         self.title(App.APP_NAME)
         self.geometry(str(App.WIDTH) + "x" + str(App.HEIGHT))
         self.minsize(App.WIDTH, App.HEIGHT)
         self.folder = ""
+        self.out_geojson = ""
         self.zip_files = []
         self.number_of_files = ""
         self.coordinates = []
         self.canvas_coordinates = []
-        self.point_counter = 1 
+        self.point_counter = 1
 
-#       Logo Frame
-        #path = "sentinel_quick_search_logo.gif"
-        #self.image = tk.PhotoImage(file=path)
-        #self.larger_image = self.image.zoom(2, 2)
-        #self.smaller_image = self.image.subsample(8, 8)
-        #self.logo = Label(
-        #    master, image=self.smaller_image).pack(
-        #    side=TOP, padx=5)
-        
-        #self.black_space_00 = Label(self, text="").pack(side=TOP, padx=5)
+        # User inputs
+        self.password = "password"
+        self.username = "username"
+        self.input_path = "" 
+        self.output_folder = ""
+        orbit_direction = "" 
+        start_date = "" 
+        end_date = ""
+
 
         # ================= CTkFrames ============================== # 
         self.grid_columnconfigure(0, weight = 0)
@@ -64,28 +63,47 @@ class App(ctk.CTk):
 
         self.frame_left = ctk.CTkFrame(master=self, 
                                         width = 150)
-        self.frame_left.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+
+        self.frame_left.grid(row=0,
+                            column=0,
+                            padx=20,
+                            pady=20,
+                            sticky="nsew")
 
         self.frame_right = ctk.CTkFrame(master=self, 
                                         corner_radius=10)
-        self.frame_right.grid(row=0, column=1, rowspan=1, pady=20, padx=20, sticky="nsew")
+        self.frame_right.grid(row=0,
+                            column=1,
+                            rowspan=1,
+                            pady=20,
+                            padx=20,
+                            sticky="nsew")
+
+        self.frame_bottom = ctk.CTkFrame(master=self, 
+                                        corner_radius=5)
+        self.frame_bottom.grid(row=2,
+                                column=0,
+                                padx=20,
+                                pady=10,
+                                sticky="nsew",
+                                columnspan=2)
         
         #  Map View and Map Functionalities
-        #self.canvas = ctk.CTkCanvas(self, width=800, height=500, bg="black", relief=SUNKEN)
-        #self.canvas.pack(side=TOP)
         self.map_widget = TkinterMapView(self.frame_right, 
-                                        width=450, 
+                                        width=500, 
                                         height=250, 
                                         corner_radius=10, 
-                                        relief=SUNKEN)      
+                                        relief=SUNKEN,
+                                        )      
+        
         self.map_widget.grid(row=0, 
                             rowspan=1, 
-                            column=0, 
-                            columnspan=3, 
+                            column=0,
+                            columnspan=2, 
                             sticky="nswe", 
                             padx=20, 
                             pady=20)
-        
+
         self.map_widget.add_right_click_menu_command(label = "Add Point",
                                                     command = self.add_marker_event,
                                                     pass_coords=True)
@@ -94,143 +112,65 @@ class App(ctk.CTk):
                                                     command = self.create_polygon,
                                                     pass_coords=False)
 
-        #self.black_space_00_1 = Label(self, text="").pack(side=TOP, padx=5)
-
+        self.user_name_entry = Entry(self.frame_left, width=20, justify="center")
+        self.user_name_entry.insert(END, self.username)
+        self.user_name_entry.pack(side=TOP)
+        self.password_entry = Entry(self.frame_left, width=20, justify="center")
+        self.password_entry.insert(END, self.password)
+        self.password_entry.pack(side=TOP)
+        #self.password_entry.grid(padx=5, pady=10, sticky="nswe", row=0, column=0)
+        #self.password_entry.place(anchor = CENTER)
+        #self.user_name_entry.grid(padx=5, pady=10, sticky="nswe", row=1, column=0)
+        #self.user_name_entry.place(anchor = CENTER)
+        #self.user_name_entry.place(relx=.5, rely=.5, anchor = CENTER)
         
-        # Frame: browse folder
-#        self.search_folder_frame = ctk.CTkFrame(self, width=150, height=30,
-#                                         relief=GROOVE, borderwidth=4)
-#
-#        self.blank_label = Label(
-#            self.search_folder_frame,
-#            text="").pack(
-#            side=TOP,
-#            padx=5)
-#
-#        self.search_button = ctk.CTkButton(
-#            self.search_folder_frame,
-#            text="Open..",
-#            command=self.browse_folder).pack(
-#            side=LEFT,
-#            padx=5,
-#            pady=10)
-#
-#        self.display_folder = Label(self.search_folder_frame, width=80,
-#                                    bg="white", textvariable=self.folder,
-#                                    relief=SUNKEN, anchor=W)
-#
-#        self.display_folder.pack(side=LEFT)
-#
-#        self.input_folder_label = Label(
-#            self.search_folder_frame,
-#            text="Input Folder",
-#            relief=GROOVE).place(
-#            relx=0.70,
-#            rely=0.20,
-#            anchor=W)
-#
-#        self.search_folder_frame.pack(side=TOP)
-#
-#        self.black_space_01 = Label(self, text="").pack(side=TOP, padx=5)
-#
-#        # Frame: number of files
-#
-#        self.number_files_frame = ctk.CTkFrame(self, width=150, height=30,
-#                                        relief=GROOVE, borderwidth=4)
-#
-#        self.list_files_button = ctk.CTkButton(
-#            self.number_files_frame,
-#            text="Search .zip",
-#            command=self.list_tar).pack(
-#            side=LEFT,
-#            padx=5,
-#            pady=10)
-#
-#        self.display_num_zips = Label(
-#            self.number_files_frame,
-#            width=10,
-#            bg="white",
-#            textvariable=self.number_of_files,
-#            relief=SUNKEN,
-#            anchor=W)
-#
-#        self.display_num_zips.pack(side=LEFT)
-#
-#        self.number_files_frame.pack(side=TOP)
-#
-#        self.blank_space_02 = Label(self, text="").pack(side=TOP, padx=5)
-#
-#        self.run_button_frame = ctk.CTkFrame(self, width=150, height=30,
-#                                      relief=GROOVE, borderwidth=4)
-#        self.run_button = ctk.CTkButton(self.run_button_frame, text="Run",
-#                                 command=self.unzip_files).pack(
-#                side=LEFT, padx=5, pady=10)
-#
-#        self.run_button_frame.pack(side=TOP)
-#
-#
-#    def browse_folder(self):
-#        
-#        """Command to browse folder."""
-#
-#        folder_name = askdirectory()
-#        self.folder = folder_name
-#        self.display_folder.config(text=folder_name)
-#
-#    def list_tar(self):
-#
-#        """Creates a list of zip files."""
-#
-#        files = [
-#            os.path.join(
-#                root, file) for root, subdir, files in os.walk(
-#                self.folder) for file in files if file.endswith('.zip')]
-#        self.zip_files = files
-#        self.display_num_zips.config(text=str(len(self.zip_files)))
-#        print(self.zip_files)
-#        print(str(len(self.zip_files)))
-#
-#        return files
-#
-#    def unzip_files(self):
-#
-#        """Unzip files from a list."""
-#
-#        if len(self.zip_files) == 0:
-#            messagebox.showerror("Error", "No zip files in this folder")
-#
-#        else:
-#            for file in self.zip_files:
-#                zip_obj = ZipFile(file, 'r')
-#                zip_obj.extractall(self.folder)
-#                print(f"extracted {file}\n{'-'*75}")
-#
-#
-#    def update_path(self, close_path=False):
-#
-#        """Updates path based on coordinates stored as markers"""
-#        if close_path == True:
-#            x = self.coordinates[0][0]
-#            y = self.coordinates[0][1]
-#            self.map_widget.set_marker(x, y) 
-#            self.canvas_coordinates.append(self.osm_to_decimal(x,y,self.map_widget.zoom))
-#            self.coordinates.append((x,y))
-#            self.map_widget.set_path(self.coordinates)
-#        else: 
-#            self.map_widget.set_path(self.coordinates)
-#
-#
-#    def osm_to_decimal(self, tile_x: int, tile_y: int, zoom: int) -> tuple:
-#        
-#        """ converts internal OSM coordinates to decimal coordinates """
-#
-#        n = 2.0 ** zoom
-#        lon_deg = tile_x / n * 360.0 - 180.0
-#        lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * tile_y / n)))
-#        lat_deg = math.degrees(lat_rad)
-#        return lat_deg, lon_deg
-#
-#
+        self.search_button = ctk.CTkButton(
+            self.frame_bottom,
+            text="Open..",
+            command=self.save_file).grid(
+            padx=5,
+            pady=10,
+            row=1,
+            column=0,
+            columnspan=1)
+
+
+        self.display_file = Entry(self.frame_bottom, width=80,
+                                    bg="white" ,textvariable=self.out_geojson)
+        self.display_file.grid(padx=5, pady=10, sticky="w", row=1, column=1)
+
+
+    def save_file(self):
+        filename = filedialog.asksaveasfile(mode='w', defaultextension=".geojson").name
+        self.display_file.delete(0, END)
+        self.display_file.insert(0, filename)
+
+
+    def update_path(self, close_path=False):
+
+        """Updates path based on coordinates stored as markers"""
+        if close_path == True:
+            x = self.coordinates[0][0]
+            y = self.coordinates[0][1]
+            self.map_widget.set_marker(x, y) 
+            self.canvas_coordinates.append(self.osm_to_decimal(x,y,self.map_widget.zoom))
+            self.coordinates.append((x,y))
+            self.map_widget.set_path(self.coordinates)
+        else: 
+            self.map_widget.set_path(self.coordinates)
+
+
+    def osm_to_decimal(self, tile_x: int, tile_y: int, zoom: int) -> tuple:
+        
+        """ converts internal OSM coordinates to decimal coordinates """
+
+        n = 2.0 ** zoom
+        lon_deg = tile_x / n * 360.0 - 180.0
+        lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * tile_y / n)))
+        lat_deg = math.degrees(lat_rad)
+        return lat_deg, lon_deg
+
+
     def add_marker_event(self, coords):
         x = coords[0]
         y = coords[1] 
